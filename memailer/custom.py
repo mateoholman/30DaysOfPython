2f9823fd23df6c84188dc54fba8acc302ba3b69a
Matthew Holman <mateoholman@gmail.com>
2018-07-04 13:03:17 -0500

    Emailer basic setup

--- memailer/custom.py
+++ memailer/custom.py
@@ -1,4 +1,21 @@
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException
+
 host = "smtp.gmail.com"
 port = 587
 username = "thebiznis@gmail.com"
 password = "8532thebiznis"
+from_email = username
+to_list = ["hungrypy@gmail.com", "mateoholman@gmail.com"]
+msg_greeting = "Hello there, this is a test e-mail"
+
+SMTP = SMTP(host, port)
+SMTP.ehlo()
+SMTP.starttls()
+try:
+    SMTP.login(username, password)
+    SMTP.sendmail(from_email, to_list, msg_greeting)
+except SMTPAuthenticationError:
+    print("Could not login")
+except:
+    print("An error occured")
+SMTP.quit()
